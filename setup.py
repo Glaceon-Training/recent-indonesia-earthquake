57a07508b1967d52c44382c0135db0b6d264dce9
glaceon033 <glaceon033@gmail.com>
2024-08-28 22:08:39 +0700

    version 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="latterearthquake-indonesia",
-    version="0.1.0",
+    version="0.1.1",
     author="Ryandri",
     author_email="radhinusa@gmail.com",
     description="This package will get recent earthquake detected from"
